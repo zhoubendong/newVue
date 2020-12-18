764cf430f6ef4249166df40783561d820c49f2cf
zhoubd <zhoubd@zonekey.com.cn>
2020-12-18 19:02:40 +0800

    升级vue-router

--- src/views/page/pageB.vue
+++ src/views/page/pageB.vue
@@ -17,26 +17,28 @@ export default {
     setup() {
         const store = useStore(); // 使用vuex
         console.log(store);
-        const name = computed(
-            () => store.state.name
-        );
-        const age = computed(
-            () => store.state.age
-        );
+        // const name = computed(
+        //     () => store.state.name
+        // );
+        // const age = computed(
+        //     () => store.state.age
+        // );
         // const age = toRef(store.state, 'age');
-        // const { name, age } = toRefs(store.state);
+        const { name, age } = toRefs(store.state);
 
-        // const { setAge } = toRefs(store.mutations);
-        // console.log(store);
+        const { setAge } = toRefs(store._mutations);
+        console.log(store);
+        console.log(setAge.value);
 
         function addAge() {
-            store.commit('setAge', '20');
-            console.log(age);
+            // store.commit('setAge', '20');
+            // age.value = '30';
+            setAge.value[0]('40');
+            // console.log(age);
         } 
         return {
             name,
             age,
-            // state,
             addAge
         }
     }
